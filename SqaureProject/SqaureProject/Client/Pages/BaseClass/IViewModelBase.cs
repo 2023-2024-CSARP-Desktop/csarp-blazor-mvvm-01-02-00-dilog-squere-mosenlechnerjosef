e9fc71e676838773452cf5da3b7d21926256f27c
Mösenlechner Josef <mosenlechnerjosef@gmail.com>
2023-09-26 11:36:07 +0200

    comit

---
+++ SqaureProject/SqaureProject/Client/Pages/BaseClass/IViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿﻿using System.ComponentModel;
+
+namespace ViewModels.BaseClass
+{
+    public interface IViewModelBase
+    {
+        public bool IsBusy { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
